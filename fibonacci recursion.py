ab46926a3c575af22cff0ebf7ed677ddd0239d31
krishaybhardwaj <63946047+krishaybhardwaj@users.noreply.github.com>
2021-10-19 17:44:54 +0530

    Add files via upload

---
+++ fibonacci recursion.py
@@ -0,0 +1,16 @@
+y = int(input("Enter any number: "))
+def fibonacci(x):
+    count = 0
+    term_1 = 0
+    term_2 = 1
+    while count < x:
+        print(term_1)
+        term_x = term_1 + term_2
+        term_1 = term_2
+        term_2 = term_x
+        count+=1
+    return count
+print(fibonacci(y))
+
+
+
