958f2b878a118540d7e9848209d6c048182bd17f
krishaybhardwaj <63946047+krishaybhardwaj@users.noreply.github.com>
2021-10-20 22:28:34 +0530

    Add files via upload

--- number guessing game.py
+++ number guessing game.py
@@ -3,8 +3,10 @@ import random
 user_input = int(input("Enter a number: "))
 
 random_number = random.randint(0, user_input)
+guesses = 0
 
 while True:
+    guesses +=1 
     guess = int(input("Guess the number - "))
 
     if guess == random_number:
@@ -13,8 +15,9 @@ while True:
     
     elif guess > random_number:
         print("Go lower")
-        
+                
     else:
         print("Go higher")
-    
-    
+       
+        
+print("You took", guesses, "guesses")
