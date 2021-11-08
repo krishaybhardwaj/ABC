cc33e45ae6db8a5df28c23b9c22154d293e44622
krishaybhardwaj <63946047+krishaybhardwaj@users.noreply.github.com>
2021-11-08 14:59:38 +0530

    Add files via upload

--- multiple elements in a list.py
+++ multiple elements in a list.py
@@ -1,21 +1,12 @@
-# make your own list
-list = []
-user_input = int(input("How long should the list be? "))  
+import random
 
-steps = 1
-for i in range(0, user_input ):
-    element = int(input(f'Enter element number {steps}: '))
-    steps+=1
+list = [random.randrange(1, 21, 1) for i in range(10)]
 
-    list.append(element) 
-print(f'The list you made: {list}\n')
+print (f'Random number list is : {list}')
 
-
-# find how many times an element has occured
-x = int(input("Enter any element from the list you just made: "))
-count = 0
-for y in list:
-    if(y==x):
-        count+=1
-        
-print(f'{x} has occured {count} times')
+list_k=[]
+for i in list:
+    if i not in list_k:
+        list_k.append(i)
+    else:
+        print(i)
